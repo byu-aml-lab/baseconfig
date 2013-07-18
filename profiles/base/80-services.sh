4be7c5975436b75a880dc5b4670c408cbbc3377d
Andrew McNabb <amcnabb@mcnabbs.org>
2013-07-17 21:01:59 -0600

    Add some more services to be disabled

--- profiles/base/80-services.sh
+++ profiles/base/80-services.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+# Disable the initial configuration (at first boot after install).
+systemctl disable firstboot-graphical
+systemctl disable initial-setup-graphical
+systemctl disable initial-setup-text
+
+# Disable firewalld, which is currently bad (maybe reconsider later).
 systemctl disable firewalld
+
+# Disable sendmail, which no one uses.
+systemctl disable sendmail
